eb9159fec3b86732993d199caa71600191f2f9c3
Begley Brothers Inc <begleybrothers@gmail.com>
2022-06-16 12:41:08 +1000

    Evolve[Infra]: Template status update script
    Signed-off-by: Begley Brothers Inc <begleybrothers@gmail.com>

--- test/helper/common-setup.bash
+++ test/helper/common-setup.bash
@@ -12,9 +12,6 @@ _common_setup() {
     fi
     export ORIGINAL_XDG_STATE_HOME=${XDG_STATE_HOME-}
 
-    PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 2>&1 && pwd )"
-    export RTM_TEMPLATE_URL="${PROJECT_ROOT}/test/fixtures/template.git"
-    export RTM_TEMPLATE_DATA=$(mktemp --directory)
     # get the containing directory of this file
     # use $BATS_TEST_FILENAME instead of ${BASH_SOURCE[0]} or $0,
     # as those will point to the bats executable's location or the preprocessed file respectively
